b891a0e18b80ecc2268d055465c675b2692a72fb
Brais <braisarco@gmail.com>
2025-09-03 01:41:36 +0200

    intento de configuracion de rutas

--- angular-testing/src/app/app-routing.module.ts
+++ angular-testing/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 
 const routes: Routes = [
+   {
+    path: '',
+    redirectTo: '/presentation',
+    pathMatch: 'full'
+  },
   {
-    path: 'auth',
-    loadChildren: () => import('./features/auth/auth-module').then(m => m.AuthModule)
+    path: 'presentation',
+    loadChildren: () => import('./features/presentation/presentation-module').then(m => m.PresentationModule)
   },
   {
     path: 'dashboard',
