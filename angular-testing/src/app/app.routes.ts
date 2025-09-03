a08445f5c3726c821e38bc832d127fb4691ae7f4
Brais <braisarco@gmail.com>
2025-09-03 19:37:14 +0200

    rutas "arregladas"

--- angular-testing/src/app/app.routes.ts
+++ angular-testing/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    redirectTo: '/presentation',
+    pathMatch: 'full'
+  },
+  {
+    path: 'presentation',
+    loadChildren: () => import('./features/presentation/presentation-module').then(m => m.PresentationModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./features/dashboard/dashboard-module').then(m => m.DashboardModule)
+  }];
